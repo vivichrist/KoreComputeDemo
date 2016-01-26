94a8550a33ec78d0aeb14cb8532a8d810466c8c8
Robert Konrad <robert@ktx-software.com>
2016-01-27 00:49:06 +0100

    Updated tesselation control shader to portable syntax.

--- Sources/test.tesc.glsl
+++ Sources/test.tesc.glsl
@@ -11,12 +11,13 @@ uniform float TessLevelOuter;
 
 #define ID gl_InvocationID
 
+void patch_main() {
+	gl_TessLevelInner[0] = TessLevelInner;
+	gl_TessLevelOuter[0] = TessLevelOuter;
+	gl_TessLevelOuter[1] = TessLevelOuter;
+	gl_TessLevelOuter[2] = TessLevelOuter;
+}
+
 void main() {
-    tcPosition[ID] = vPosition[ID];
-    if (ID == 0) {
-        gl_TessLevelInner[0] = TessLevelInner;
-        gl_TessLevelOuter[0] = TessLevelOuter;
-        gl_TessLevelOuter[1] = TessLevelOuter;
-        gl_TessLevelOuter[2] = TessLevelOuter;
-    }
+	tcPosition[ID] = vPosition[ID];
 }
